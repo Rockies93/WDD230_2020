04cbe255337d9f6d7bd614dc7bdafcebe9248d77
Raquel Eyler <raquel.e.badal@gmail.com>
2020-06-10 14:44:20 -0600

    Files for Table Build

---
+++ lesson8/js/lesson5.js
@@ -0,0 +1,25 @@
+//Responsive Menu
+const hambutton = document.querySelector('.hamburger');
+const mainnav = document.querySelector('.navigation');
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')});
+
+//Date at bottom
+const day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthStuff = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let numberDate = new Date();
+let theDay = day[numberDate.getDay()];
+console.log(theDay);
+let theDate = numberDate.getDate();
+let theMonth = monthStuff[numberDate.getMonth()];
+let theYear = numberDate.getFullYear();
+document.getElementById('date').textContent = theDay + ", " + theDate +" " + theMonth + " " + theYear;
+
+//Responive Banner 
+const friday = document.querySelector('.breakfast')
+
+let gettingTheDate = new Date();
+let  dayOfTheWeek = gettingTheDate.getDay();
+if (dayOfTheWeek == 5) {
+    friday.classList.toggle('breakfast-gone');
+}
